b8538c322f8cab7dd56e0c7c40c3d281d87170ff
daniel <duque1daniel@gmail.com>
2020-10-06 12:28:29 -0700

    added js project working

---
+++ script.js
@@ -0,0 +1,23 @@
+const unitSelection = document.querySelector('#unit')
+const distanceInput = document.querySelector('#distance')
+const sizeInput = document.querySelector('#tank')
+const efficiencyTotal = document.querySelector('#efficiency')
+
+function calculateEfficiency() {
+    const unitValue = unitSelection.options[unitSelection.selectedIndex].value;
+    const distanceValue = parseFloat(distanceInput.value)
+    const sizeValue = parseFloat(sizeInput.value)
+    const totalValue = distanceValue / sizeValue
+    if (unitValue == "mpg") {
+        efficiencyTotal.innerHTML = `${totalValue} mpg`
+    } else {
+        efficiencyTotal.innerHTML = `${totalValue} km/L`
+    }
+
+
+
+}
+
+distanceInput.addEventListener('input', calculateEfficiency)
+sizeInput.addEventListener('input', calculateEfficiency)
+unitSelection.addEventListener('input', calculateEfficiency)
