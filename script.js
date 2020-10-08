db7bdd9c200ac6954f5017856864c8c77cc7dbac
daniel <duque1daniel@gmail.com>
2020-10-08 13:52:15 -0700

    added flex project working

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ const distanceInput = document.querySelector('#distance')
 const sizeInput = document.querySelector('#tank')
 const efficiencyTotal = document.querySelector('#efficiency')
 
+//-------------------------------------------------------------
+
 function calculateEfficiency() {
     const unitValue = unitSelection.options[unitSelection.selectedIndex].value;
     const distanceValue = parseFloat(distanceInput.value)
     const sizeValue = parseFloat(sizeInput.value)
-    const totalValue = distanceValue / sizeValue
+    const totalValue = parseInt(distanceValue / sizeValue)
     if (unitValue == "mpg") {
         efficiencyTotal.innerHTML = `${totalValue} mpg`
     } else {
@@ -17,7 +19,7 @@ function calculateEfficiency() {
 
 
 }
-
+//-------------------------------------------------------------
 distanceInput.addEventListener('input', calculateEfficiency)
 sizeInput.addEventListener('input', calculateEfficiency)
 unitSelection.addEventListener('input', calculateEfficiency)
